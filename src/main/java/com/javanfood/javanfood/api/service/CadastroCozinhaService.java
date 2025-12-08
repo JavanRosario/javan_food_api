3bddd9ee2a3543ad690b92503d2b36b741adef88
JavanRosario <oliveiraajavan@hotmail.com>
2025-12-08 19:40:04 -0300

    refatorando metodo excluir

--- src/main/java/com/javanfood/javanfood/api/service/CadastroCozinhaService.java
+++ src/main/java/com/javanfood/javanfood/api/service/CadastroCozinhaService.java
@@ -1,8 +1,12 @@
 package com.javanfood.javanfood.api.service;
 
-import com.javanfood.javanfood.domain.model.Cozinha;
 import com.javanfood.javanfood.api.repository.CozinhaRepository;
+import com.javanfood.javanfood.domain.exeption.EntidadeEmUsoExeption;
+import com.javanfood.javanfood.domain.exeption.EntidadeNãoEncontradaExeption;
+import com.javanfood.javanfood.domain.model.Cozinha;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -13,5 +17,17 @@ public class CadastroCozinhaService {
 
     public Cozinha salvar(Cozinha cozinha) {
         return cozinhaRepository.adicionar(cozinha);
+    }
+
+    public void excluir(Long cozinhaId) {
+        try {
+            cozinhaRepository.delete(cozinhaId);
+        } catch (EmptyResultDataAccessException e) {
+            throw new EntidadeNãoEncontradaExeption("Não existe cadastro de cozinha com código: " + cozinhaId);
+        } catch (DataIntegrityViolationException e) {
+            throw new EntidadeEmUsoExeption
+                    ("Cozinha de código " + cozinhaId + " não pode ser removida. Está em uso");
+        }
+
     }
 }
