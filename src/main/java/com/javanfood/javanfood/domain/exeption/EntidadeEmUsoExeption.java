6f666577fe94b7c4d44a2bfc212768bd87aba290
JavanRosario <oliveiraajavan@hotmail.com>
2025-12-08 19:41:54 -0300

    criando exeptions para metodos

---
+++ src/main/java/com/javanfood/javanfood/domain/exeption/EntidadeEmUsoExeption.java
@@ -0,0 +1,7 @@
+package com.javanfood.javanfood.domain.exeption;
+
+public class EntidadeEmUsoExeption extends RuntimeException {
+    public EntidadeEmUsoExeption(String msg) {
+        super(msg);
+    }
+}
---
+++ src/main/java/com/javanfood/javanfood/domain/exeption/EntidadeNãoEncontradaExeption.java
@@ -0,0 +1,7 @@
+package com.javanfood.javanfood.domain.exeption;
+
+public class EntidadeNãoEncontradaExeption extends RuntimeException {
+    public EntidadeNãoEncontradaExeption(String message) {
+        super(message);
+    }
+}
