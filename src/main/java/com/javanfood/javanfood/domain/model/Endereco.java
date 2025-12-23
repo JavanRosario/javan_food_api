a75534aeed0a82cc458ce853b1152dbfcc7d1e2d
JavanRosario <oliveiraajavan@hotmail.com>
2025-12-23 08:32:35 -0300

    add lazy

--- src/main/java/com/javanfood/javanfood/domain/model/Endereco.java
+++ src/main/java/com/javanfood/javanfood/domain/model/Endereco.java
@@ -4,6 +4,7 @@ import org.hibernate.annotations.Comment;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import lombok.Data;
@@ -26,7 +27,7 @@ public class Endereco {
 	@Column(name = "endereco_bairro")
 	private String bairro;
 
-	@ManyToOne
+	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "endereco_cidade_id")
 	private Cidade cidade;
 }
