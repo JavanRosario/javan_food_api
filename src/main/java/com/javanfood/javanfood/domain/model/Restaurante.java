2a4d17900db4e12b3224baecd2de83d2a2426612
JavanRosario <oliveiraajavan@hotmail.com>
2025-12-24 12:20:46 -0300

    melhorando manytomany

--- src/main/java/com/javanfood/javanfood/domain/model/Restaurante.java
+++ src/main/java/com/javanfood/javanfood/domain/model/Restaurante.java
@@ -61,7 +61,7 @@ public class Restaurante {
 	private LocalDateTime dataAtualizacao;
 
 
-	@ManyToMany(fetch = FetchType.EAGER)
+	@ManyToMany
 	@JsonIgnore
 	@JoinTable(name = "restaurante_forma_pagamento", joinColumns = @JoinColumn(name = "restaurante_id"), inverseJoinColumns = @JoinColumn(name = "forma_pagamento_id"))
 	private List<FormaPagamento> formasPagamento = new ArrayList<>();
