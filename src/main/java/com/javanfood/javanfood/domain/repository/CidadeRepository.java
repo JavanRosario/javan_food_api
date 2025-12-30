8079054433e8f3191063b56eb017bdb048f3847c
JavanRosario <oliveiraajavan@hotmail.com>
2025-12-30 00:52:57 -0300

    voltando padrao

--- src/main/java/com/javanfood/javanfood/domain/repository/CidadeRepository.java
+++ src/main/java/com/javanfood/javanfood/domain/repository/CidadeRepository.java
@@ -8,6 +8,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 @Repository
 public interface CidadeRepository extends JpaRepository<Cidade, Long> {
- public static final List<Cidade> findByNameCidades = null;
-
 }
