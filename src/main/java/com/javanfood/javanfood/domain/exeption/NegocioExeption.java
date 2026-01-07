98285d216391f909dbbb2793814123d139667d9a
JavanRosario <oliveiraajavan@hotmail.com>
2026-01-07 00:14:11 -0300

    refactor/feat: mudando hierarquia das exeptions, add construtores

--- src/main/java/com/javanfood/javanfood/domain/exeption/NegocioExeption.java
+++ src/main/java/com/javanfood/javanfood/domain/exeption/NegocioExeption.java
@@ -5,7 +5,11 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.BAD_REQUEST)
 public class NegocioExeption extends RuntimeException {
-    public NegocioExeption(String message) {
-        super(message);
-    }
+	public NegocioExeption(String message) {
+		super(message);
+	}
+
+	public NegocioExeption(String msg, Throwable causa) {
+		super(msg, causa);
+	}
 }
