36eec19629b61c3ae0fbcf67c58449556d3b339d
JavanRosario <oliveiraajavan@hotmail.com>
2026-01-08 08:30:58 -0300

    refactor: removendo @responsestatus

--- src/main/java/com/javanfood/javanfood/domain/exeption/EntidadeEmUsoExeption.java
+++ src/main/java/com/javanfood/javanfood/domain/exeption/EntidadeEmUsoExeption.java
@@ -1,9 +1,5 @@
 package com.javanfood.javanfood.domain.exeption;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(HttpStatus.CONFLICT)
 public class EntidadeEmUsoExeption extends NegocioExeption {
 	public EntidadeEmUsoExeption(String msg) {
 		super(msg);
