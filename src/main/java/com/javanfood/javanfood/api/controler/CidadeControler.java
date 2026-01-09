594a790fbdf465a5911af66026be8d90a883d3c4
JavanRosario <oliveiraajavan@hotmail.com>
2026-01-09 08:29:08 -0300

    refactor: removendo imports

--- src/main/java/com/javanfood/javanfood/api/controler/CidadeControler.java
+++ src/main/java/com/javanfood/javanfood/api/controler/CidadeControler.java
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.javanfood.javanfood.api.exeptionhandler.Problema;
+import com.javanfood.javanfood.api.exeptionhandler.Problem;
 import com.javanfood.javanfood.domain.exeption.EntidadeNaoEncontradaExeption;
 import com.javanfood.javanfood.domain.exeption.EstadoNaoEncontradoExeption;
 import com.javanfood.javanfood.domain.exeption.NegocioExeption;
