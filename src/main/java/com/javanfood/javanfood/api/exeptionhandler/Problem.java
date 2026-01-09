2ee43dde84283e56b2d93897eb16e4c69e3e47c4
JavanRosario <oliveiraajavan@hotmail.com>
2026-01-09 08:28:52 -0300

    feat: mudando classe Problem para seguir padrao rfc 7807

--- src/main/java/com/javanfood/javanfood/api/exeptionhandler/Problem.java
+++ src/main/java/com/javanfood/javanfood/api/exeptionhandler/Problem.java
@@ -1,14 +1,18 @@
 package com.javanfood.javanfood.api.exeptionhandler;
 
-import java.time.LocalDateTime;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
 
 import lombok.Builder;
 import lombok.Getter;
-
+@JsonInclude(Include.NON_NULL)
 @Getter
 @Builder
-public class Problema {
-	private LocalDateTime dataHora;
-	private String menssagem;
+public class Problem {
+	private Integer status;
+	private String type;
+	private String title;
+	private String detail;
+
 }
- 
+
