5a9e83911db7d8ee41935e8b0d28b0e1f7ce34fb
JavanRosario <oliveiraajavan@hotmail.com>
2026-07-10 20:15:44 -0300

    teste branch

--- src/main/java/com/javanfood/javanfood/api/dto/response/PedidoResumoResponse.java
+++ src/main/java/com/javanfood/javanfood/api/dto/response/PedidoResumoResponse.java
@@ -1,16 +1,11 @@
 package com.javanfood.javanfood.api.dto.response;
 
-import com.fasterxml.jackson.annotation.JsonFilter;
-
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.List;
 
 //@JsonFilter("pedidoFilter")
 public record PedidoResumoResponse(String codigo, BigDecimal subTotal, BigDecimal taxaFrete, BigDecimal valorTotal,
-                                   String statusPedido, LocalDateTime dataCriacao,
-                                   RestauranteResumidoPedidoResponse restaurante,
-                                   UsuarioSemSenhaResponse cliente,
-                                   List<ItemPedidoResponse> itens
-) {
+		String statusPedido, LocalDateTime dataCriacao, RestauranteResumidoPedidoResponse restaurante,
+		UsuarioSemSenhaResponse cliente, List<ItemPedidoResponse> itens) {
 }
